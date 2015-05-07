7c6404579957b2097cb7da54cd05b810e167cfbb
Paul Thiebauth <paulmthiebauth@gmail.com>
2015-05-07 00:00:21 -0400

    cleanup movies.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,11 +16,8 @@ class MoviesController < ApplicationController
     movie_info = Imdb::Movie.new(params[:id])
     @actor_and_index = {}
     movie_info.cast_members[0..4].each_with_index {|actor, index| @actor_and_index[actor] = index}
-    #SEND
     @actor_names = @actor_and_index.keys
-    #SEND
     @actor_and_index = @actor_and_index.to_a.shuffle
-    #SEND
     @actor_and_image = {}
     @actor_names.each do |name|
       image_url = Google::Search::Image.new(:query => name).first.uri
